ceda4a60105b7df8c75625dcdfdae3b95e9e0a54
Dilmurod_Ravshanov <169117371+DilmurodDev-FS@users.noreply.github.com>
2026-05-28 13:02:38 +0500

    Variable.py files added in Python Lesson Repo

---
+++ variable(o'zgaruvchilar).py
@@ -0,0 +1,16 @@
+# Sana 28:05:26 yil
+# Ustoz : Anvar Narzullayev
+# Mavzu: Variable ( o'zgaruvchilar )
+
+# O'zgaruvchi bu - ma'lumotlarni saqlash uchun mo'ljallangan joy. O'zgaruvchilar yordamida biz ma'lumotlarni saqlaymiz va ularni dastur davomida ishlatamiz.
+
+ismlar = "Anvar, Nodir, Diyorbek"
+yoshlar = "50, 25, 30"
+print(ismlar)
+print(yoshlar)
+a = 10
+b = 20
+c = a + b
+print("a ning qiymati ", a)
+print("b ning qiymati ", b)
+print("a + b = ning qiymati ", c)
