c004ed1756455cbf2906455012979fecc9f584de
Dilmurod_Ravshanov <169117371+DilmurodDev-FS@users.noreply.github.com>
2026-05-31 09:57:54 +0500

    updated code

--- for_loop.py
+++ for_loop.py
@@ -13,15 +13,22 @@ mehmonlar = ["Anvar", "Abbos", "Aziz", "Asad"]
 #for mehmon in mehmonlar:
  #   print(f"Assalomu alaykum, {mehmon} xush kelibsiz!")
 
-sonlar = list(range(11))
+""" sonlar = list(range(11))
 sonning_kvadrati = []
 for son in sonlar:
     sonning_kvadrati.append(son**2)
 print(f"Sonlar:{sonlar}")
-print(f"Sonlarning kvadrati: {sonning_kvadrati}")
+print(f"Sonlarning kvadrati: {sonning_kvadrati}") """
 
+dostlar =[]
 
+print("Eng yaqin 5 ta do'stingizni kiriting: ")
 
+for n in range(5):
+
+    dostlar.append(input(f"{n+1} chi do'stingizni ismini kiriting: ").capitalize())
+
+print(f"Do'stlaringiz ro'yhati: {dostlar}")
 
 
 
