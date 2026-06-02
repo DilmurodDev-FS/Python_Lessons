be7b21367cd3ebd7c11a491f83d1995ce863efbc
Dilmurod_Ravshanov <169117371+DilmurodDev-FS@users.noreply.github.com>
2026-06-02 17:28:54 +0500

    updated code

--- tuples.py
+++ tuples.py
@@ -7,13 +7,53 @@ Ustoz: Anvar Narzullayev
 Mavzu: O'zgarmas ro'yhatlar bilan ishlash (Tuples)
 
 """
-cars = ['Bmw', 'damas', 'kobalt', 'Mersedes', 'audi', 'Neksiya'] # Ro'hatdagi katta harflar kichik harflardan oldin keladi. Shuning uchun ro'yhatdagi elementlarni oldin kichik harfga o'tkazib olish kerak.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#cars = ['Bmw', 'damas', 'kobalt', 'Mersedes', 'audi', 'Neksiya'] # Ro'hatdagi katta harflar kichik harflardan oldin keladi. Shuning uchun ro'yhatdagi elementlarni oldin kichik harfga o'tkazib olish kerak.
 #cars.sort(reverse=True) #sort() metodi ro'yxatni alifbo tartibida tartiblaydi.
 #print(sorted(cars, reverse=True)) #sorted() metodi ro'yxatni alifbo tartibida tartiblaydi va ro'yhatni o'zgartirmaydi. reverse=True parametri yordamida ro'yhatni teskari tartibda chiqarish mumkin.
 #cars.reverse() #reverse() metodi ro'yhatni teskari tartibda chiqaradi.
 #print(len(cars))
-sonlar = list(range(0, 10)  ) #range() metodi yordamida 0 dan 100 gacha bo'lgan sonlarni ro'yhatga aylantirish mumkin. range() metodi uchta parametr qabul qiladi: boshlang'ich qiymat, tugash qiymati va qadam. Agar qadam ko'rsatilmasa, u 1 ga teng bo'ladi.
-print(sonlar)
+#sonlar = list(range(0, 10)  ) #range() metodi yordamida 0 dan 100 gacha bo'lgan sonlarni ro'yhatga aylantirish mumkin. range() metodi uchta parametr qabul qiladi: boshlang'ich qiymat, tugash qiymati va qadam. Agar qadam ko'rsatilmasa, u 1 ga teng bo'ladi.
+#print(sonlar)
 
 
 
